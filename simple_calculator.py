4c75f2c73e960a2666217f2af8563243de86bcb9
resisttheflow0-png <resisttheflow0@gmail.com>
2026-05-26 12:00:31 +0300

    deleted the main.py file and update the calculator

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,61 +1,57 @@
-print("Welcome to the calculator!")
-
-print(" If you want to get started, type 'go' below/ " \
+print("Welcome to the calculator!" \
+"\n If you want to get started, type 'go' below." \
 "\n If you want to exit, type 'exit' below.")
 
 while True:
-    user_input_menu = input("--- ")
-    if user_input_menu == "go":
-            
+        print("|menu|")
+        user_input_menu = input("--- ")
+    
+        if user_input_menu == "go":
                 print("Great! \n Let's get started. " \
                 "\n Below, I'll ask you to write down the numbers you want to perform the operation on, "
                 "as well as the operations themselves.")
+        
+                while True:        
+                        print("Enter the first number.")
+                        first_num = int(input("--- "))
+
+                        print("Enter the second number.")
+                        second_num = int(input("--- "))
+
+                        print("Enter an action.")
+                        action = input("--- ")
+
+                        while second_num == 0 and action in ("/", "divide"):
+                                print("You cannot divide be zero." \
+                                "\nEnter the second number.")
+                                second_num = int(input("--- "))
+
+                        if action == "divide" or action == "/":
+                                print("Answer:", first_num / second_num)
                 
-                print("Enter the first number.")
-                first_num = int(input("--- "))
-
-                print("Enter the second number.")
-                second_num = int(input("--- "))
-
-                print("Enter an action.")
-            
-                action = input("--- ")
-
-                if action == "plus" or action == "+":
-                    total = first_num + second_num
-                    print(total)
-                    
-                elif action == "subtract" or action == "-":
-                        total = first_num - second_num
-                        print(total)
-                            
-                elif action == "divide" or action == "/":
-                        total = first_num / second_num
-                        print(total)
-                                    
-                elif action == "multiply" or action == "*":
-                            total = first_num * second_num
-                            print(total)
-                            
-                        
-                else:
-                        print("I don't understand.")
-                        
-                print("Shall we carry on?")
-
-
+                        elif action == "plus" or action == "+":
+                                print("Answer:", first_num + second_num)
                 
-                user_input_calcul = input("--- ")
-                if user_input_calcul == "yes":
-                        print("Greate!")
+                        elif action == "subtract" or action == "-":
+                                print("Answer:", first_num - second_num)
+                
+                        elif action == "multiply" or action == "*":
+                                print("Answer:", first_num * second_num)
+
+                        print("Shall we carry on?")
+                        user_input_calcul = input("--- ")
             
-                elif user_input_calcul == "no":
-                                exit()
-                    
-                            
+                        while user_input_calcul not in ("yes", "no"):
+                                print("I don't understand." \
+                                "\nShall we carry on?")
+                                user_input_calcul = input("--- ")
+            
+                        if user_input_calcul == "yes":
+                                continue
+                        elif user_input_calcul == "no":
+                                break
 
-
-    elif user_input_menu == "exit":
-        exit()
-    else:
-        print("I don't understand.")
+        elif user_input_menu == "exit":
+                break
+        else:
+                print("I don't understand.")
