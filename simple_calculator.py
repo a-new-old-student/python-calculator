a2a8845119aab6bcba731de31e4e0eab3e9680d7
resisttheflow0-png <resisttheflow0@gmail.com>
2026-05-26 20:27:20 +0300

    upgrate

--- simple_calculator.py
+++ simple_calculator.py
@@ -11,32 +11,60 @@ while True:
                 "\n Below, I'll ask you to write down the numbers you want to perform the operation on, "
                 "as well as the operations themselves.")
         
-                while True:        
-                        print("Enter the first number.")
-                        first_num = int(input("--- "))
+                while True:
+                        while True:
+                                try:        
+                                        print("Enter the first number.")
+                                        first_num = int(input("--- "))
+                                        break                        
+                                except ValueError:
+                                        print("This is not a number! Please use digits.")
+                        while True:
+                                try:
+                                        print("Enter the second number.")
+                                        second_num = int(input("--- "))
+                                        break
+                                except ValueError:
+                                        print("This is not a number! Please use digits.")
 
-                        print("Enter the second number.")
-                        second_num = int(input("--- "))
 
                         print("Enter an action.")
                         action = input("--- ")
 
+                        
+                        while action not in ("divide", "/", "plus", "+", "subtract", "-", "multiply", "*", "degree", "**"):
+                                print("Please enter the correct action.")
+                                action = input("--- ")
+
+
+                        
+                                
                         while second_num == 0 and action in ("/", "divide"):
-                                print("You cannot divide be zero." \
-                                "\nEnter the second number.")
-                                second_num = int(input("--- "))
+                                while True:
+                                        try:
+                                                print("You cannot divide be zero." \
+                                                "\nEnter the second number.")
+                                                second_num = int(input("--- "))
+                                                break
+                                        except ValueError:
+                                                print("This is not a number! Please use digits.")
+                                
+                               
+
 
                         if action == "divide" or action == "/":
-                                print("Answer:", first_num / second_num)
+                                print(f"Answer: {first_num / second_num}")
                 
                         elif action == "plus" or action == "+":
-                                print("Answer:", first_num + second_num)
+                                print(f"Answer: {first_num + second_num}")
                 
                         elif action == "subtract" or action == "-":
-                                print("Answer:", first_num - second_num)
+                                print(f"Answer: {first_num - second_num}")
                 
                         elif action == "multiply" or action == "*":
-                                print("Answer:", first_num * second_num)
+                                print(f"Answer: {first_num * second_num}")
+                        elif action == "degree" or action == "**":
+                                print(f"Answer: {first_num ** second_num}")
 
                         print("Shall we carry on?")
                         user_input_calcul = input("--- ")
